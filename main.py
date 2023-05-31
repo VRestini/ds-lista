e4730c6fd1a1dd3b2f552900add3478ff2402e74
Vitor e Gabrella <vitorsouzalibrelonrestini@gmail.com>
2023-05-31 09:20:09 -0300

    a

---
+++ main.py
@@ -0,0 +1,52 @@
+# o método list() cria uma lista vazia
+lista = ["Edson", 12, 1.67, True, 34]
+#print(lista)
+
+#lista.append('Novo')
+#print(lista)
+
+#lista.insert(3, '2AI')
+#print(lista)
+
+#lista.pop(2)
+#print(lista)
+
+#lista.remove('Edson')
+#print(lista)
+
+#x = 12
+#indice = lista.index(x)
+#print(indice)
+#print(f"O elemento '{x}' está no índice '{indice}'")
+
+qtd = lista.count(34)
+tam = len(lista)
+print(qtd)
+print(tam)
+
+# soma = sum(lista)
+
+# lista.sort(reverse=True)
+
+#lista.reverse()
+#print(lista)
+
+#lista.clear()
+del(lista)
+print(lista)
+
+#l1 = [1, 2, 3]
+#l2 = [4, 5, 6]
+#l3 = l1 + l2
+#print(l1)
+#print(l2)
+#print(l3)
+#lista1 = [1,2,3]
+#print(f"lista1 = {lista1}")
+"""
+lista2 = [4,5,6]
+"""
+#lista2 = lista1.copy() # copy copia os valores, se usar = os dois vetores estarão na mesma posição da memória, ou seja, se eu mudar um valor do vetor 1, mudará no vetor 2
+#print(f"lista2 = {lista2}")
+
+
